94c7835c69bf504bc917bcb8c32dbd0acfd3f371
cashman694 <psh971001@gmail.com>
2024-06-27 15:10:12 +0900

    CardView수정
    CardView 수정하고
    CardMasterDatabase에 카드 데이터 추가했습니다.
    확인 부탁드립니다!

--- Assets/App/Scripts/Battle/Views/CardView.cs
+++ Assets/App/Scripts/Battle/Views/CardView.cs
@@ -1,14 +1,27 @@
 using App.Battle.Interfaces.Views;
+using App.Common.Data.MasterData;
+using TMPro;
 using UnityEngine;
 
 namespace App.Battle.Views
 {
     public class CardView : MonoBehaviour, ICardView
     {
+        [SerializeField] TMP_Text _CardNameTMP;
+        [SerializeField] TMP_Text _HPTMP;
+        [SerializeField] TMP_Text _CardNumberTMP;
         public void Init(string id, string name, int level, int MaxHp) => throw new System.NotImplementedException();
 
         public void UpdateHp(int hp) => throw new System.NotImplementedException();
 
+        public CardMasterData CardMasterData;
+        public void Setup(CardMasterData cardMasterData)
+        {
+            this.CardMasterData = cardMasterData;
+            _CardNameTMP.text= cardMasterData.Name;
+            _HPTMP.text = cardMasterData.Hp.ToString();
+            _CardNumberTMP.text = cardMasterData.CardNumber.ToString();
+        }
         public void Unspawn()
         {
             Destroy(gameObject);
