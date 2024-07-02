5bc9bb9faa1bd4737dd199b940da66f806fd6585
mc_cho <kutar777@gmail.com>
2024-07-02 23:58:08 +0900

    DeckCardView의 수정내용을 리버트

--- Assets/App/Scripts/Battle/Interfaces/Views/IDeckCardView.cs
+++ Assets/App/Scripts/Battle/Interfaces/Views/IDeckCardView.cs
@@ -1,14 +1,7 @@
-using App.Common.Data;
-using Unity;
-using System;
-using UnityEngine;
-
 namespace App.Battle.Interfaces.Views
 {
     public interface IDeckCardView
     {
-        void Setup(string cardNumber, string name, CardLevel cardLevel, int maxHp, Sprite Sprite );
-        void UpdateHp(int hp);
         void Unspawn();
     }
 }
