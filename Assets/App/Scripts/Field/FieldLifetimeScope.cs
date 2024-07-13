d6febe27fc1e1dc2f1257cfc2384382732aabf0e
mc_cho <kutar777@gmail.com>
2024-07-14 01:02:22 +0900

    Area씬을 Field로 이름변경

--- Assets/App/Scripts/Field/FieldLifetimeScope.cs
+++ Assets/App/Scripts/Field/FieldLifetimeScope.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
-namespace App.Area
+namespace App.Field
 {
-    public sealed class AreaLifetimeScope : LifetimeScope
+    public sealed class FieldLifetimeScope : LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
