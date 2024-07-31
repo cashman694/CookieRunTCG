099b38d1e8ec52acc1febaba9629dca3d78ba9af
mc_cho <kutar777@gmail.com>
2024-08-01 02:16:40 +0900

    덱 빌드 방법을 변경

--- Assets/App/Scripts/Battle/UseCases/PlayerCardUseCase.cs
+++ Assets/App/Scripts/Battle/UseCases/PlayerCardUseCase.cs
@@ -2,6 +2,7 @@ using App.Battle.Interfaces.DataStores;
 using App.Battle.Interfaces.UseCases;
 using App.Common.Data;
 using App.Common.Data.MasterData;
+using System.Linq;
 using VContainer;
 using VContainer.Unity;
 
@@ -34,11 +35,31 @@ namespace App.Battle.UseCases
         {
             var cardsLength = _CardMasterDatabase.Cards.Length;
 
-            for (int i = 0; i < _BattleConfig.DeckCount; i++)
+            while (_PlayerCardDataStore.Cards.Count() < _BattleConfig.DeckCount)
             {
                 var randomNumber = UnityEngine.Random.Range(0, cardsLength);
                 var cardMaster = _CardMasterDatabase.Cards[randomNumber];
-                var id = i.ToString();
+
+                // 쿠키카드는 반드시 1장 이상 포함되어야 한다
+                if (!_PlayerCardDataStore.Cards.Any() && cardMaster.CardType != CardType.Cookie)
+                {
+                    continue;
+                }
+
+                // 같은 넘버의 카드는 최대 4장까지 넣을 수 있다
+                if (_PlayerCardDataStore.Cards.Count(x => x.CardNumber == cardMaster.CardNumber) >= 4)
+                {
+                    continue;
+                }
+
+                // FLIP 카드는 최대 16장까지 넣을 수 있다
+                if (_PlayerCardDataStore.Cards.Count(x => x.CardMasterData.HasFlipEffect) >= 16)
+                {
+                    continue;
+                }
+
+                var count = _PlayerCardDataStore.Cards.Count();
+                var id = (++count).ToString();
 
                 _PlayerCardDataStore.AddCard(id, cardMaster);
             }
