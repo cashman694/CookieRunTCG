896baa89d5c9c6b57e488a417b25915fedde50d7
mc_cho <kutar777@gmail.com>
2024-08-01 00:01:54 +0900

    마스터데이터에 플립효과 속성을 추가

--- Assets/App/Scripts/Common/Data/MasterData/CardMasterData.cs
+++ Assets/App/Scripts/Common/Data/MasterData/CardMasterData.cs
@@ -9,6 +9,7 @@ namespace App.Common.Data.MasterData
         [field: SerializeField] public string Name { get; private set; }
         [field: SerializeField] public int Level { get; private set; }
         [field: SerializeField] public int Hp { get; private set; }
+        [field: SerializeField] public bool HasFlipEffect { get; private set; }
         [field: SerializeField] public CardType CardType { get; private set; }
         [field: SerializeField] public EnergyType EnergyType { get; private set; }
         [field: SerializeField] public Rarity Rarity { get; private set; }
